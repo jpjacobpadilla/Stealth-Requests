bebd005a7fdba77020b052d7d276e69d8dbff6dd
Jacob Padilla <jp@jacobpadilla.com>
2024-08-17 22:09:11 -0400

    Made new profiles

--- stealth_requests/session.py
+++ stealth_requests/session.py
@@ -1,4 +1,6 @@
 import random
+import json
+from dataclasses import dataclass
 from urllib.parse import urlparse
 from collections import defaultdict
 from functools import partialmethod
@@ -9,88 +11,33 @@ from curl_cffi.requests.session import Session, AsyncSession
 from curl_cffi.requests.models import Response
 
 
-PROFILES = {
-    'chrome124': [
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        },
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        },
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        },
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        },
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        },
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        },
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        },
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        },
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        },
-        {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"',
-            "sec-ch-ua-mobile": '?0',
-            "sec-ch-ua-platform": '"macOS"'
-        }
-    ]
-}
+@dataclass
+class ClientProfile:
+    user_agent: str
+    sec_ch_ua: str
+    sec_ch_ua_mobile: str
+    sec_ch_ua_platform: str
 
 
 class BaseStealthSession:
     def __init__(
             self, 
-            user_agent: str = None,
-            impersonate: str = 'chrome124', 
+            client_profile: str = None,
+            impersonate: str = 'chrome_124', 
             **kwargs
         ):
-        if impersonate == 'chrome':
-            impersonate = 'chrome124'
+        if impersonate.lower() in ('chrome', 'chrome124'):
+            impersonate = 'chrome_124'
+        elif impersonate.lower() in ('safari', 'safari17_0', 'safari17'):
+            impersonate = 'safari_17_0'
 
-        self.profile = random.choice(PROFILES[impersonate])
-        self.user_agent = user_agent
+        self.profile = client_profile or self.create_profile(impersonate)
         self.last_request_url = defaultdict(lambda: 'https://www.google.com/')
         
         super().__init__(
-            headers=self.initialize_headers(),
+            headers=self.initialize_chrome_headers() 
+                if impersonate == 'chrome_124' 
+                else self.initialize_safari_headers(),
             impersonate=impersonate, 
             **kwargs
         )
@@ -108,8 +55,21 @@ class BaseStealthSession:
     async def __aexit__(self, *_):
         self.close()
         return False
+    
+    def create_profile(impersonate: str) -> ClientProfile:
+        with open('.profiles.json', encoding='utf-8', mode='r') as file:
+            user_agents = json.load(file)
 
-    def initialize_headers(self) -> dict[str, str]:
+        assert impersonate in user_agents.keys(), f'Please choose one of the supported profiles: {user_agents.keys()}'
+
+        return ClientProfile(
+            user_agent=random.choice(user_agents[impersonate]),
+            sec_ch_ua='"Not A;Brand";v="99", "Chromium";v="124", "Google Chrome";v="124"' if impersonate == 'chrome_124' else None,
+            sec_ch_ua='?0' if impersonate == 'chrome_124' else None,
+            sec_ch_ua='"macOS"' if impersonate == 'chrome_124' else None
+        )
+        
+    def initialize_chrome_headers(self) -> dict[str, str]:
         return {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "Accept-Encoding": "gzip, deflate, br, zstd",
@@ -118,14 +78,28 @@ class BaseStealthSession:
             "Connection": "keep-alive",
             "Pragma": "no-cache",
             "Upgrade-Insecure-Requests": "1",
-            "User-Agent": self.profile['User-Agent'],
+            "User-Agent": self.profile.user_agent,
             "Sec-Fetch-Dest": "document",
             "Sec-Fetch-Mode": "navigate",
             "Sec-Fetch-Site": "same-origin",
             "Sec-Fetch-User": "?1",
-            "sec-ch-ua": self.profile['sec-ch-ua'],
-            "sec-ch-ua-mobile": self.profile['sec-ch-ua-mobile'],
-            "sec-ch-ua-platform": self.profile['sec-ch-ua-platform'],
+            "sec-ch-ua": self.profile.sec_ch_ua,
+            "sec-ch-ua-mobile": self.profile.sec_ch_ua_mobile,
+            "sec-ch-ua-platform": self.profile.sec_ch_ua_platform,
+        } 
+
+    def initialize_safari_headers(self) -> dict[str, str]:
+        return {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Pragma": "no-cache",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-origin",
+            "User-Agent": self.profile.user_agent
         } 
 
     def get_dynamic_headers(self, url: str) -> dict[str, str]:
@@ -136,9 +110,6 @@ class BaseStealthSession:
             "Host": host,
             "Referer": self.last_request_url[host]
         }
-
-        if self.user_agent:
-            headers['User-Agent'] = self.user_agent
 
         self.last_request_url[host] = url
         return headers
